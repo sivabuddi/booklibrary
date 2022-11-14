f2b78b9d50f4bfbf02184b1290d8d8e2be605ec6
sivabuddi <sivakumar.umkc.fall2019@gmail.com>
2022-11-14 09:08:22 -0600

    Java Bean class converted to Database table using @Entity and Persistence API using ORM(Object Relational Mapping). Add lombok dependencies in pom.xml.

---
+++ src/main/java/com/learn/booklibrary/model/Library.java
@@ -0,0 +1,28 @@
+package com.learn.booklibrary.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Library {
+
+    @Id
+    private Integer libraryId;
+
+    private String studentName;
+    private String branch;
+    private String rank;
+
+
+
+}
