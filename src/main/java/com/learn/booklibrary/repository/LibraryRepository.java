cc4b7cdde12d6c8374fcc73a53212730036c2c21
sivabuddi <sivakumar.umkc.fall2019@gmail.com>
2022-11-15 15:37:08 -0600

    LibraryRepository which implements JpaRepository interface for CRUD operations.

---
+++ src/main/java/com/learn/booklibrary/repository/LibraryRepository.java
@@ -0,0 +1,10 @@
+package com.learn.booklibrary.repository;
+
+import com.learn.booklibrary.model.Library;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LibraryRepository extends JpaRepository<Library,Integer> {
+
+}
