6a883828f63d37fdd3fbb4d4cef20e57b371f3f2
sivabuddi <sivakumar.umkc.fall2019@gmail.com>
2022-11-15 15:34:00 -0600

    Business Logic for jpa repository for CRUD operations. Here we are using h2 database;

--- src/main/java/com/learn/booklibrary/service/LibraryService.java
+++ src/main/java/com/learn/booklibrary/service/LibraryService.java
@@ -1,45 +1,52 @@
 package com.learn.booklibrary.service;
 
 import com.learn.booklibrary.model.Library;
+import com.learn.booklibrary.repository.LibraryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class LibraryService {
 
-    List<Library> libraryList = new ArrayList<>(Arrays.asList(
-            new Library(001,"SivaKumar","CSE",500),
-            new Library(002,"Rakesh","IT",900),
-            new Library(003, "Kenny","EIE",800)));
+//    List<Library> libraryList = new ArrayList<>(Arrays.asList(
+//            new Library(001,"SivaKumar","CSE",500),
+//            new Library(002,"Rakesh","IT",900),
+//            new Library(003, "Kenny","EIE",800)));
 
 
     // display all books
 
+    @Autowired private  LibraryRepository libraryRepository;
+
     public List<Library> getAllBooks(){
-        return this.libraryList;
+        return this.libraryRepository.findAll();
+        //return this.libraryList;
     }
 
 
 
     // display only single book
 
-    public Library getSingleBook(Integer id){
-        for(int i=0;i<this.libraryList.size();i++){
-            Library library1 = this.libraryList.get(i);
-            if(library1.getLibraryId().equals(id))
-                return library1;
-        }
-        return null;
+    public Optional<Library> getSingleBook(Integer id){
+        return this.libraryRepository.findById(id);
+//        for(int i=0;i<this.libraryList.size();i++){
+//            Library library1 = this.libraryList.get(i);
+//            if(library1.getLibraryId().equals(id))
+//                return library1;
+//        }
+//        return null;
     }
 
 
     // add new book
 
     public String addNewBook(Library library){
-        this.libraryList.add(library);
+        this.libraryRepository.save(library);
         return "Added new Book Successfully";
    }
 
@@ -47,17 +54,19 @@ public class LibraryService {
     // update book with new student id
 
     public void updateBook(Library library, Integer libraryId ){
-        for(int i=0;i<this.libraryList.size();i++){
-            Library library1 = this.libraryList.get(i);
-            if(library1.getLibraryId().equals(libraryId))
-                this.libraryList.set(i,library);
-        }
+//        for(int i=0;i<this.libraryList.size();i++){
+//            Library library1 = this.libraryList.get(i);
+//            if(library1.getLibraryId().equals(libraryId))
+//                this.libraryList.set(i,library);
+//        }
+        this.libraryRepository.save(library);
 
     }
 
     // delete a book
     public String deleteBook(Integer libraryId){
-        this.libraryList.removeIf(book -> book.getLibraryId().equals(libraryId));
+//        this.libraryList.removeIf(book -> book.getLibraryId().equals(libraryId));
+        this.libraryRepository.deleteById(libraryId);
         return libraryId + "Library Id successfully";
     }
 
