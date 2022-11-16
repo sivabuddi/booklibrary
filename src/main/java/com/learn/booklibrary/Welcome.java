eb275fe8f8a50e8453c2cc18ef899b9c08673004
sivabuddi <sivakumar.umkc.fall2019@gmail.com>
2022-11-16 10:02:18 -0600

    @Profile annotation been used to enabled environment.

--- src/main/java/com/learn/booklibrary/Welcome.java
+++ src/main/java/com/learn/booklibrary/Welcome.java
@@ -1,10 +1,12 @@
 package com.learn.booklibrary;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Profile("dev")
 public class Welcome {
 
     @Value("${spring.message}")
